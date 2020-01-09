2fc95c3497fd3342a1342523e98808ce8caeab93
jonathonflorek <jonathonflorek@gmail.com>
2020-01-09 00:28:36 -0600

    install react

--- src/renderer/index.ts
+++
@@ -1,3 +0,0 @@
-import * as path from 'path'
-
-alert(path.join('a', 'b'));
---
+++ src/renderer/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+ReactDom.render(<>Hello Express</>, document.getElementById('root'))
